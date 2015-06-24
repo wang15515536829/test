00f2346a67c8a4187bbb7caf829a4d267969ea04
mutouren <994217300@qq.com>
2015-06-24 15:40:47 +0800

    Initial Commit

---
+++ jinmei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jinmei
+//
+//  Created by lanouhn on 15/6/24.
+//  Copyright (c) 2015年 lanouhn3g - 23 .com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jinmei/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jinmei
+//
+//  Created by lanouhn on 15/6/24.
+//  Copyright (c) 2015年 lanouhn3g - 23 .com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
